06b71c7bca4c0a2b8695384effd590b2dd09c2ba
vmuravskyi <v.muravskyi@connectwise.com>
2022-10-03 23:30:40 +0300

    bean scope

--- src/main/java/com/muravskyi/spring/spring_introduction/Dog.java
+++ src/main/java/com/muravskyi/spring/spring_introduction/Dog.java
@@ -2,8 +2,18 @@ package com.muravskyi.spring.spring_introduction;
 
 public class Dog implements Pet {
 
+    private String name;
+
     public Dog() {
         System.out.println("Dog bean is created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
--- src/test/java/com/muravskyi/spring/spring_introduction/Test4.java
+++ src/test/java/com/muravskyi/spring/spring_introduction/Test4.java
@@ -10,8 +10,12 @@ public class Test4 {
         ClassPathXmlApplicationContext context =
             new ClassPathXmlApplicationContext("applicationContextBeanScope.xml");
         Dog myDog = context.getBean("myPet", Dog.class);
+        myDog.setName("Jack");
         Dog yourDog = context.getBean("myPet", Dog.class);
+        yourDog.setName("Oliver");
 
+        System.out.println(myDog.getName());
+        System.out.println(yourDog.getName());
         System.out.println(myDog == yourDog);
     }
 
