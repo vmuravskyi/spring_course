cfa2410e0bcb55d19ca3571d73b19351a6ca3cec
vmuravskyi <v.muravskyi@connectwise.com>
2022-10-07 18:20:39 +0300

    add hibernate support

---
+++ src/main/java/com/muravskyi/spring/section03_hibernate/hibernate_test/Employee.java
@@ -0,0 +1,96 @@
+package com.muravskyi.spring.section03_hibernate.hibernate_test;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+
+    public Employee() {
+        // default
+    }
+
+    public Employee(String name, String surname, String department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Employee setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Employee setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Employee setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Employee setDepartment(String department) {
+        this.department = department;
+        return this;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public Employee setSalary(int salary) {
+        this.salary = salary;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("id", id)
+            .append("name", name)
+            .append("surname", surname)
+            .append("department", department)
+            .append("salary", salary)
+            .toString();
+    }
+
+}
