c4932e2332d771aef87f3a379e2be17a50319ef1
muravskyi <v.muravskyi@gmail.com>
2022-10-23 01:12:10 +0300

    rest done without client

--- src/main/java/com/muravskyi/spring/section06_spring_rest/spring_rest/src/main/java/com/muravskyi/spring/rest/controller/TheController.java
+++ src/main/java/com/muravskyi/spring/section06_spring_rest/spring_rest/src/main/java/com/muravskyi/spring/rest/controller/TheController.java
@@ -1,11 +1,10 @@
 package com.muravskyi.spring.rest.controller;
 
 import com.muravskyi.spring.rest.entity.Employee;
+import com.muravskyi.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.muravskyi.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +18,42 @@ public class TheController {
     @GetMapping("/employees")
     public List<Employee> showAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException(String.format("Employee with id %s was not found", id));
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveOrUpdate(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees/{id}")
+    public Employee updateNewEmployee(@PathVariable int id, @RequestBody Employee employee) {
+        Employee isEmployeeExist = employeeService.getEmployee(id);
+        if (isEmployeeExist == null) {
+            throw new NoSuchEmployeeException(String.format("Employee with id %s was not found", id));
+        }
+        employee.setId(id);
+        employeeService.saveOrUpdate(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee isEmployeeExist = employeeService.getEmployee(id);
+        if (isEmployeeExist == null) {
+            throw new NoSuchEmployeeException(String.format("Employee with id %s was not found", id));
+        }
+        employeeService.deleteEmployee(id);
+        return String.format("Employee with id %s was successfully deleted", id);
     }
 
 }
---
+++ src/main/java/com/muravskyi/spring/section06_spring_rest/spring_rest/src/main/java/com/muravskyi/spring/rest/exception_handling/EmployeeIncorrectData.java
@@ -0,0 +1,19 @@
+package com.muravskyi.spring.rest.exception_handling;
+
+public class EmployeeIncorrectData {
+
+    private String info;
+
+    public EmployeeIncorrectData() {
+        // default
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+}
