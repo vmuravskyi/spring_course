f699e990d168c7580ea284ed101e47ab9013fcf0
muravskyi <v.muravskyi@gmail.com>
2022-10-24 14:33:40 +0300

    spring security done

--- src/main/java/com/muravskyi/spring/section07_spring_security/spring_security/src/main/java/com/muravskyi/spring/security/configuration/Config.java
+++ src/main/java/com/muravskyi/spring/section07_spring_security/spring_security/src/main/java/com/muravskyi/spring/security/configuration/Config.java
@@ -1,11 +1,15 @@
 package com.muravskyi.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "com.muravskyi.spring.security")
@@ -15,11 +19,23 @@ public class Config {
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setPrefix("/WEB-INF/view");
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
     }
 
-
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?allowPublicKeyRetrieval=true");
+            dataSource.setUser("root");
+            dataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return dataSource;
+    }
 
 }
--- src/main/java/com/muravskyi/spring/section07_spring_security/spring_security/src/main/java/com/muravskyi/spring/security/configuration/WebSecurityConfig.java
+++ src/main/java/com/muravskyi/spring/section07_spring_security/spring_security/src/main/java/com/muravskyi/spring/security/configuration/WebSecurityConfig.java
@@ -1,20 +1,40 @@
 package com.muravskyi.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("volodymyr").password("admin").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("oliver").password("admin").roles("HR"))
-                .withUser(userBuilder.username("jack").password("admin").roles("MANAGER", "HR"));
+//        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("volodymyr").password("admin").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("oliver").password("admin").roles("HR"))
+//                .withUser(userBuilder.username("jack").password("admin").roles("MANAGER", "HR"));
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("hr_info").hasRole("HR")
+                .antMatchers("manager_info/**").hasRole("MANAGER")
+                .and()
+                .formLogin()
+                .permitAll();
     }
 
 }
